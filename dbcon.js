faf985fb77af1968a54ad6524f5d83d8f51db85c
Charles Jadin <jadinc@oregonstate.edu>
2018-02-19 09:59:26 -0600

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_jadinc',
-  password        : 'Fade2blk',
-  database        : 'cs290_jadinc'
+  host            : '<HOSTNAME>',
+  user            : '<USERNAME>',
+  password        : '<PASSWORD>',
+  database        : '<DB>'
 });
 
 module.exports.pool = pool;
